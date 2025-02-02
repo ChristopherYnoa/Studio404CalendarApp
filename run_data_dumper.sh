5fda0f16b77b0a41cf79e770d560baa4d03eb00e
Chris Ynoa <92812488+ChristopherYnoa@users.noreply.github.com>
2025-02-01 23:05:56 -0500

    automating the program execution
    program creates a virtual environment, installs required libraries, runs, and then uninstalls the virtual environment

--- run_data_dumper.sh
+++ run_data_dumper.sh
@@ -7,5 +7,18 @@ rm sheet_token.json
 
 echo "Executing Python script..."
 
+python3 -m venv venv
 
+
+echo "Entering venv..."
+source venv/bin/activate
+echo "installing required libraries..."
+
+pip install -r requirements.txt
+
+echo "running program..."
 python3 main.py
+
+echo "program finished running, removing venv..."
+
+rm -rf venv
